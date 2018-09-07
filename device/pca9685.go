3374951c3bf10799ed3a915ceb635d182c76a4c7
kuai6 <kuai6@ya.ru>
2018-09-07 16:10:10 +0300

    i2c implementation

--- device/pca9685.go
+++ device/pca9685.go
@@ -28,8 +28,8 @@ const (
 	DEFAULT_FREQ float32 = 60.0
 	OSC_FREQ     float32 = 25000000.0
 	STEP_COUNT   float32 = 4096.0
-	MIN_PULSE    float32 = 150
-	MAX_PULSE    float32 = 650
+	MIN_PULSE    float32 = 120
+	MAX_PULSE    float32 = 620
 )
 
 type PCA9685 struct {
